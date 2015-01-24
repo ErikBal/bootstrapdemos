1bb87d6fc4e08fbed389f19c4af6ace8b815c48e
Erik Balonier <erikibali@gmail.com>
2015-01-24 13:06:11 -0600

    second commit

--- views/variables.php
+++ views/variables.php
@@ -1,10 +1,37 @@
 <section id="variables">
 	<article>
+		<h2> Variables </h2>
 		<p>
 			I am creating easy Variables firstly.
 		</p>
 		<p>
 			I used @example-color 2 times now.
 		</p>
+		<p class="hover">
+			And even hover with this color.
+		</p>
 	</article>
+</section>
+<section id="interpolation">
+		<h2>
+			Variable Interpolation
+		</h2>
+		<p> 
+			We used variables to work with css values.
+			We can also use variables on selector names, property names, Urls and @import(s)
+		</p>
+		<p class="blackbox">
+			The class I am using for this p-tag is "blackbox", but in my less file I use @bbox: blackbox;
+		</p>
+		<article>
+			Here , I use @pic: "../images"; to have a faster way to get to "images"-folder.
+		</article>
+		<p>
+			I changed the "color"-property to "farbe".-----><br />
+			p{<br />
+			@{farbe}: @example-color-gre;<br />
+		</p>
+		<p>
+			vgthrt
+		</p>
 </section>
