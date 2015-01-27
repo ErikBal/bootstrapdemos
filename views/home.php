0e7873c94aa29c8fb2ecfd04371c4db9e80360b3
Erik Balonier <erikibali@gmail.com>
2015-01-27 16:14:38 -0600

    forth

--- views/home.php
+++ views/home.php
@@ -5,7 +5,6 @@
 			<strong>
 				I am trying to get more comfortable with LESS.<br />
 				I am using the Less Website to improve and understand less better.<br />
-				I will start at the Variables and work down along the structure.
 			</strong>
 		</p>
 	</article>
--- views/variables.php
+++ views/variables.php
@@ -1,41 +1,89 @@
 <section id="variables">
-	<article>
 
-		<h2> 
-			Variables 
-		</h2>
+<h2> 
+Variables 
+</h2>
 
-		<p>
-			I am creating easy Variables firstly.
-		</p>
-		<p>
-			I used @example-color 2 times now.
-		</p>
-		<p class="hover">
-			And even hover with this color.
-		</p>
+<p>
+I am creating easy Variables firstly.
+</p>
+<p>
+I used @example-color 2 times now.
+</p>
+<p class="hover">
+And even hover with this color.
+</p>
+<pre>
+pre{
+	h2{
+		color: @example-color-gre;
+		text-align: center;
+		}
+	h2:hover{
+		color: darken(@example-color-gre, 10%)
+		}
+	p{
+		color: @example-color-gre;
+		text-align: center;
+		}
+	p{
+		color: @example-color-gre;
+		}
+	.hover:hover{
+		color: lighten(@example-color-gre, 30%)
+		}
+	}	
+}
 
-	</article>
+		/*Variables*/
+
+
+	@example-color-gre: #53b91b;
+	@example-color-yel: #fcda1f;
+	@example-color-or: #fbb714;
+	@example-color-bla: #000000;
+</pre>
 </section>
 <section id="interpolation">
+<h2>
+Variable Interpolation
+</h2>
 
-		<h2>
-			Variable Interpolation
-		</h2>
+<p id="interpolation-p1"> 
+We used variables to work with css values.
+We can also use variables on selector names, property names, Urls and @import(s)
+</p>
+<br />
+<br />
+<p class="blackbox">
+The class I am using for this p-tag is "blackbox", but in my less file I use @bbox: blackbox;
+</p>
+<pre>
+.@{bbox} {
+	background-color: @example-color-bla;
+	color: @example-color-or;
 
-		<p id="interpolation-p1"> 
-			We used variables to work with css values.
-			We can also use variables on selector names, property names, Urls and @import(s)
-		</p>
+	/*Variable*/
+	@bbox: blackbox;
+</pre>
+<article>
+Here , I use @pic: "../images"; to have a faster way to get to "images"-folder.
+Afterwards I just have to select the file "less.png"
+Example below:
 
-		<p class="blackbox">
-			The class I am using for this p-tag is "blackbox", but in my less file I use @bbox: blackbox;
-		</p>
 
-		<article>
-			Here , I use @pic: "../images"; to have a faster way to get to "images"-folder.
-		</article>
+</article>
+<pre>							
+		article{
+		background: url("@{pic}/less.png");
+		background-repeat: no-repeat;
+		background-size: 50%;
+		background-position: center;
+												}
 
+											Variable
+											@pic: "../images";
+</pre>
 		<p id="interpolation-p2">
 			I changed the "color"-property to "farbe".-----><br />
 			p{<br />
@@ -53,12 +101,39 @@
 			Lazy Loading
 		</h2>
 
-		<p class="test1">
-			Example one: Snippet Version (variable inside less)
+		<p class="snippet">
+			Example one: Snippet Version (variable inside less)<br />
+			.snippet{<br />
+				width: @testwidth;<br />
+				@realwidth: 30%;<br /><br />
+				/*Variable*/<br />
+				@testwidth: @realwidth;
+		</p>
+		</section>
+		<code>
+			Example two: Another valid Version of Less (variables in extra section)<br />
+			.ovalid{<br />
+				width: @testwidth;<br /><br />
+				/*Variable*/<br />
+				@testwidth: @realwidth;<br />
+				@realwidth: 40%;
+		</code>
+		<p class="morevariables">
+		   In a definition the last variable is used, if its defined twice.<br />
+		That means you can easily override variables.
+		</p>
+		<p class="morevariables2">
+
 		</p>
 
-		<p class="test2">
-			Example two: Another valid Version of Less (variables in extra section)
-		</p>
+		<pre>
+			<code>
+		 		hhjhj]
+		    	kjkj 
+		      	kjkj
+		      </code>
+		</pre>
 
-</section>
+<code>
+ga
+</code>
