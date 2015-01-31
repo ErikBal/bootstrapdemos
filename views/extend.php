6e3c736b212c5e2947288153bc2443f3d06305c4
Erik Balonier <erikibali@gmail.com>
2015-01-30 19:31:53 -0600

    fifth

--- views/extend.php
+++ views/extend.php
@@ -1,1 +1,35 @@
-This is the extend page.
+<section id="extend">
+	<h1> Extend </h1>
+	<p>You can extend your selectors with <code>&:extend</code> onto your class without any reference, because it is not css.</p>
+	<p class="p1">The first sentence with a font-size of 14px, but the same font-color with .fontcolor-class.</p>
+	<p class="p2">The second sentence with a font-size of 16px, but the same font-color with .fontcolor-class.</p>
+	<p class="p3">The third sentence with a font-size of 18px, but the same font-color with .fontcolor-class.</p>
+<pre>
+.fontcolor{
+		color: @example-font-color;
+	}
+
+Now I use &:extend for .fontcolor
+
+.p1{
+ 	&:extend(.fontcolor);
+ 		font-size: 14px;
+}
+
+.p2{
+	 &:extend(.fontcolor);
+ 	font-size: 16px;
+}
+
+.p3{
+ 	&:extend(.fontcolor);
+ 	font-size: 18px;
+	}
+}
+
+r
+/*Variables*/
+@example-font-color: #2243d1;
+
+</pre>
+
--- views/variables.php
+++ views/variables.php
@@ -7,13 +7,16 @@ Variables
 <p>
 I am creating easy Variables firstly.
 </p>
+
 <p>
 I used @example-color 2 times now.
 </p>
+
 <p class="hover">
 And even hover with this color.
 </p>
 <pre>
+
 pre{
 	h2{
 		color: @example-color-gre;
@@ -44,7 +47,9 @@ pre{
 	@example-color-bla: #000000;
 </pre>
 </section>
+
 <section id="interpolation">
+
 <h2>
 Variable Interpolation
 </h2>
@@ -53,87 +58,157 @@ Variable Interpolation
 We used variables to work with css values.
 We can also use variables on selector names, property names, Urls and @import(s)
 </p>
-<br />
-<br />
+
+
 <p class="blackbox">
 The class I am using for this p-tag is "blackbox", but in my less file I use @bbox: blackbox;
 </p>
+
 <pre>
-.@{bbox} {
+.@{bbox}{
 	background-color: @example-color-bla;
 	color: @example-color-or;
-
+}
 	/*Variable*/
 	@bbox: blackbox;
+
+	@example-color-gre: #53b91b;
+	@example-color-yel: #fcda1f;
+	@example-color-or: #fbb714;
+	@example-color-bla: #000000;
 </pre>
+
 <article>
 Here , I use @pic: "../images"; to have a faster way to get to "images"-folder.
 Afterwards I just have to select the file "less.png"
 Example below:
-
-
 </article>
-<pre>							
-		article{
-		background: url("@{pic}/less.png");
-		background-repeat: no-repeat;
-		background-size: 50%;
-		background-position: center;
-												}
 
-											Variable
-											@pic: "../images";
+<pre>							
+article{
+	background: url("@{pic}/less.png");
+	background-repeat: no-repeat;
+	background-size: 50%;
+	background-position: center;
+}
+
+		  /*Variable*/
+		@pic: "../images";
 </pre>
+
+
 		<p id="interpolation-p2">
-			I changed the "color"-property to "farbe".-----><br />
-			p{<br />
-			@{farbe}: @example-color-gre;<br />
+			I changed the "color"-property to "farbe"----><br />
+<pre>
+#interpolation-p2{
+	@{farbe}: @example-color-gre;
+}
+	/*Variable*/
+	@farbe : color;
+</pre>
 		</p>
+
 
 		<p id="interpolation-p3">
 			I've given the variable a name.
-		</p>
+<pre>
+#interpolation-p3{
+	content: @@var;
+}
 
+	/*Variable*/
+	@name : "erikalexanderbalonier";
+	@var : "name";
+</pre>
+		</p>
 </section>
-<section id="lazy-loading">
 
-		<h2>
-			Lazy Loading
-		</h2>
 
-		<p class="snippet">
-			Example one: Snippet Version (variable inside less)<br />
-			.snippet{<br />
-				width: @testwidth;<br />
-				@realwidth: 30%;<br /><br />
-				/*Variable*/<br />
-				@testwidth: @realwidth;
-		</p>
-		</section>
-		<code>
-			Example two: Another valid Version of Less (variables in extra section)<br />
-			.ovalid{<br />
-				width: @testwidth;<br /><br />
-				/*Variable*/<br />
-				@testwidth: @realwidth;<br />
-				@realwidth: 40%;
-		</code>
-		<p class="morevariables">
-		   In a definition the last variable is used, if its defined twice.<br />
-		That means you can easily override variables.
-		</p>
-		<p class="morevariables2">
 
-		</p>
 
-		<pre>
-			<code>
-		 		hhjhj]
-		    	kjkj 
-		      	kjkj
-		      </code>
-		</pre>
+<section id="lazyloading">
+	<h2>
+		Lazy Loading
+	</h2>
 
-<code>
-ga
-</code>
+	<article class="snippet">
+		Example one: Snippet Version 
+<pre>
+.snippet{
+	width: @testwidth;
+	@realwidth: 30%;
+}
+
+	/*Variable*/
+	@testwidth: @realwidth;
+</pre>
+	</article>
+
+	<article class="ovalid">
+			Example two: Another valid Version of Less 
+<pre>
+.ovalid{
+	width: @testwidth;
+}
+				
+	/*Variable*/
+	@testwidth: @realwidth;
+	@realwidth: 40%;
+</pre>
+		</article>
+
+<article class="morevariables">
+	If a variable is defined twice, the last definition scope upwards is used.<br />
+<pre>
+.morevariables{
+	@var1 : 1;
+	.class1{
+		@var1 : 2;
+		.class2{
+			@var1 : 3;
+			cba : @var1;
+		}
+		abc: @var1;
+	}
+}
+</pre>
+Compiles to:
+<pre>
+.class1{
+	abc : 2;
+}
+.class1 .class2{
+	cba : 3;
+}
+</pre>
+</article>
+
+<section id="defaultvariables">
+	<h2>
+		default variables
+	</h2>
+	<p class="examplecolor">
+		You can easily override variables.<br />
+		For Example: 
+	</p>
+<pre>
+#defaultvariables{
+	color: @base-color;
+
+	
+}
+	/*Variables*/
+@base-color: #5334b7;          <strong>default</strong>
+@dark-base-color:darken(@base-color, 20%);
+@base-color: #b725a4;          <strong>override</strong>
+</pre>
+<p>
+	you can easily override the variable by putting it after the default.<br />
+	Also after <code>@import</code> of a library.<br />
+	The darker color also changes to the override.
+
+</p>
+</section>
+<p class="next">
+<a href="index.php?page=extend">NEXT</a>
+</p>
