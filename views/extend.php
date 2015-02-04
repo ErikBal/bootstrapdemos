6a930016d6fe8857f8aab3dc61d0ce23a6206612
Erik Balonier <erikibali@gmail.com>
2015-02-03 19:40:34 -0600

    extend finished soon

--- views/extend.php
+++ views/extend.php
@@ -1,4 +1,7 @@
-<section id="extend">
+
+<!--Extend introduction-->
+
+<section id="extend1">
 	<h1> <code>&:extend</code> </h1>
 	<p>You can apply your "extended"-selectors(p1, p2 ,p3) with the selector <code>&:extend</code> onto your class (.fontcolor) without any reference, because it is not css.</p>
 	<p class="p1">The first sentence with a font-size of 14px, but the same font-color with .fontcolor-class.</p>
@@ -30,8 +33,14 @@
 
 	/*Variables*/
 	@example-font-color: #2243d1;
-
 </pre>
+</section>
+
+
+
+<!--Extend Syntax-->
+
+<section id="extend2">
 	<h3>
 	<code>&:extend</code> Syntax
 	</h3>
@@ -62,7 +71,31 @@
 <span class="pre-comment">//extends only .fontcolor</span>
 }
 </pre>
-<br />
+	<p>
+	You can extend one or more classes by using a comma to seperate them:
+	</p>
+<pre>
+.a:extend(.fontcolor){
+	
+}
+.a:extend(.fontsize){
+	
+}
+<span class="pre-comment">//Both versions do the same thing:</span>
+
+.a:extend(.fontcolor, .fontsize){
+	
+}
+</pre>
+</section>
+
+
+
+
+<!--Extend attached to selector-->
+
+<section id="extend3">
+	<h3><code>&:extend</code> attached to selector</h3>
 	<p>
 		<code>&:extend</code> attached to selector, looks like a pseudo-class with selector as a parameter.<br/>
 		You can put multiple extend clauses into a selector, but you have to put the extends at the end of the selector.
@@ -72,5 +105,128 @@ pre:hover:extend(.fontcolor .fontsize)<span class="pre-comment">//After the sele
 pre:hover :extend(.fontcolor .fontsize)<span class="pre-comment">//After the selector, with a space in between</span>
 pre:hover:extend(.fontcolor .fontsize):extend(div pre)<span class="pre-comment">//Multiple extends</span>
 pre:hover:extend(.fontcolor .fontsize, div pre)<span class="pre-comment">//Multiple extends (does the same)</span>
+<span class="pre-comment"> //Extend must be last !!!</span>
 </pre>
+	<p>
+	If a ruleset contains of multiple selectors, each of the selectors can have <code>&:extend</code>.
+	</p>
+<pre>
+.a-class,
+.b-class:extend(.fontcolor),
+.c-class:extend(.fontsize){
+	<span class="pre-comment">//Body</span>
+}
+</pre>
+</section>
+
+
+
+
+<!--Extend inside ruleset-->
+
+<section id="extend4">
+	<h3>
+		<code>&:extend</code> inside ruleset
+	</h3>
+	<p>
+		You can put <code>&:extend</code> into a ruleset's body. This means you dont have to put 
+		<code>&:extend</code> after each selector of this ruleset. Both works though.<br />
+		Inside ruleset:
+	</p>
+	<pre>
+pre:hover,
+.a-class{
+	&:extend(div pre);
+} <span class="pre-comment">//Into a ruleset's body</span>
+	</pre>
+	<p>
+	After each selector:
+	</p>
+	<pre>
+pre:hover:extend(div pre),
+.a-class:extend(div pre){
+<span class="pre-comment">//Body</span>
+} <span class="pre-comment">//After each selector of this ruleset</span>
+	</pre>
+</section>
+
+
+<!--Extending nested Selectors-->
+
+<section id="extend5">
+	<h3>
+		Extending nested Selectors
+	</h3>
+	<p>
+		<code>&:extend</code> is able to match nested selectors.
+	</p>
+	<pre>
+.class{
+	article{
+		color: red;
+	}
+}
+
+.a-class:extend(.class article){
+	color: red;
+}
+	</pre>
+
+	<h5>
+		Output:
+	</h5>
+	<pre>
+.class article,
+.a-class{
+	color: red;
+}
+	</pre>
+	<p>
+		<code>&:extend</code> looks at the compiled css, not the original less
+	</p>
+	<pre>
+.class{
+	article & {
+		color: red;
+	}
+}
+
+.a-class:extend(article .class){
+	color: red;
+}
+	</pre>
+	<h5>
+		Output:
+	</h5>
+	<pre>
+article .class,
+.a-class{
+	color: red;
+}
+	</pre>
+</section>
+
+
+
+<!--Exact Matching with Extend-->
+
+<section id="extend6">
+	<h3>
+		Exact Matching with <code>&:extend</code>
+	</h3>
+	<p>
+		<code>&:extend</code> by default looks for exact match between selectors. Exact spelling matters to match selectors,
+		except quotes in attribute selectors. 
+		Leading star, order of pseudo-classes and nth-expressions form <strong>matters</strong>.<br />
+		Examples:
+	</p>
+	<pre>
+.a-class.b-class,
+.b-class.a-class,
+.b-class > a-class{
+	color: blue;
+}
+
+c-class:extend(.b-class){} <span class="pre-comment">//Will not match any selectors above</span>
+	</pre>
 </section>
